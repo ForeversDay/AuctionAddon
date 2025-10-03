fc8c8a5c63d80e6643178ab35c8d14473d9a5c73
Footiks <aleks11012011n7@inbox.ru>
2025-10-03 17:15:43 +0500

    Обновлены классы и конфиги

--- src/main/java/ru/foort/auctionaddon/commands/AhCommand.java
+++ src/main/java/ru/foort/auctionaddon/commands/AhCommand.java
@@ -1,6 +1,7 @@
 package ru.foort.auctionaddon.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -40,7 +41,8 @@ public class AhCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) return true;
+        if (!(sender instanceof Player player))
+            return true;
         if (args.length == 0) {
             var menu = menuLoader.getMenu(homeMenuId);
             if (menu != null) menu.create(player, null).open();
@@ -51,23 +53,39 @@ public class AhCommand implements CommandExecutor {
         switch (sub) {
             case "help" -> {
                 int sec = plugin.getConfig().getInt("settings.dsell_seconds", 10);
-                for (String line : plugin.getConfig().getStringList("messages.help")) player.sendMessage(Color.translate(line.replace("%sec%", String.valueOf(sec))));
+                for (String line : plugin.getConfig().getStringList("messages.help"))
+                    player.sendMessage(Color.translate(line.replace("%sec%", String.valueOf(sec))));
                 return true;
             }
             case "author", "authors" -> {
-                for (String line : plugin.getConfig().getStringList("messages.author")) player.sendMessage(Color.translate(line));
+                for (String line : plugin.getConfig().getStringList("messages.author"))
+                    player.sendMessage(Color.translate(line));
                 return true;
             }
             case "sell", "dsell" -> {
-                if (args.length < 2) { player.sendMessage(Color.translate(plugin.getConfig().getString(sub + "_usage"))); return true; }
+                if (args.length < 2) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString(sub + "_usage")));
+                    return true;
+                }
                 long priceL = utils.parseAmount(args[1]);
-                if (priceL < plugin.getConfig().getInt("settings.sell_min-price", 10)) { player.sendMessage(Color.translate(plugin.getConfig().getString("messages.min_price").replace("%min%", String.valueOf(plugin.getConfig().getInt("settings.sell_min-price", 10))))); return true; }
-                if (priceL > plugin.getConfig().getInt("settings.sell_max-price", 100000000)) { player.sendMessage(Color.translate(plugin.getConfig().getString("messages.max_price").replace("%max%", String.valueOf(plugin.getConfig().getInt("settings.sell_max-price", 100000000))))); return true; }
+                if (priceL < plugin.getConfig().getInt("settings.sell_min-price", 10)) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString("messages.min_price").replace("%min%", String.valueOf(plugin.getConfig().getInt("settings.sell_min-price", 10)))));
+                    return true;
+                }
+                if (priceL > plugin.getConfig().getInt("settings.sell_max-price", 100000000)) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString("messages.max_price").replace("%max%", String.valueOf(plugin.getConfig().getInt("settings.sell_max-price", 100000000)))));
+                    return true;
+                }
                 int price = (int) priceL;
                 ItemStack hand = player.getInventory().getItemInMainHand();
-                if (hand == null || hand.getType().isAir()) { player.sendMessage(Color.translate(plugin.getConfig().getString("messages.no_item"))); return true; }
-                if (Main.getBlackList().stream().anyMatch(tag -> hand.getType().name().equalsIgnoreCase(tag))) { player.sendMessage(Color.translate(plugin.getConfig().getString("messages.item_in_black_list"))); return true; }
-
+                if (hand == null || hand.getType().isAir()) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString("messages.no_item")));
+                    return true;
+                }
+                if (Main.getBlackList().stream().anyMatch(tag -> hand.getType().name().equalsIgnoreCase(tag))) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString("messages.item_in_black_list")));
+                    return true;
+                }
                 if (sub.equals("sell")) {
                     try {
                         User user = Main.getStorage().getUserOrCreate(player);
@@ -90,8 +108,13 @@ public class AhCommand implements CommandExecutor {
                                     .replace("%count%", String.valueOf(hand.getAmount()))
                                     .replace("%price%", String.valueOf(price));
                             player.sendMessage(Color.translate(listedMsg));
-                        } else { player.getInventory().addItem(hand); }
-                    } catch (Exception ex) { player.getInventory().addItem(hand); ex.printStackTrace(); }
+                        } else {
+                            player.getInventory().addItem(hand);
+                        }
+                    } catch (Exception ex) {
+                        player.getInventory().addItem(hand);
+                        ex.printStackTrace();
+                    }
                 } else {
                     int delay = plugin.getConfig().getInt("settings.dsell_seconds", 10);
                     Bukkit.getPluginManager().callEvent(new DSellEvent(player, hand.clone(), price, delay));
@@ -100,15 +123,24 @@ public class AhCommand implements CommandExecutor {
                 return true;
             }
             case "search" -> {
-                if (args.length < 2) { player.sendMessage(Color.translate(plugin.getConfig().getString("messages.search_usage"))); return true; }
+                if (args.length < 2) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString("messages.search_usage")));
+                    return true;
+                }
                 String input = String.join("_", Arrays.copyOfRange(args, 1, args.length)).toLowerCase(Locale.ROOT);
                 Set<String> resultTags = utils.getRuToEn().keySet().stream().filter(tag -> tag.contains(input)).collect(Collectors.toSet());
-                if (resultTags.isEmpty()) { player.sendMessage(Color.translate(plugin.getConfig().getString("messages.search_no_item"))); return true; }
+                if (resultTags.isEmpty()) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString("messages.search_no_item")));
+                    return true;
+                }
                 Category custom = Main.getCfg().getSorting().getAs("special.search", Category.class);
                 custom.setSoft(true);
                 custom.setTags(resultTags.stream().map(utils.getRuToEn()::get).collect(Collectors.toSet()));
                 var menu = menuLoader.getMenu(homeMenuId);
-                if (menu == null) { player.sendMessage(Color.translate("&fЭх, опять… То ли я что-то намудрил, то ли ты забыл в Main поменять название меню на своё.")); return true; }
+                if (menu == null) {
+                    player.sendMessage(Color.translate("&fЭх, опять… То ли я что-то намудрил, то ли ты забыл в Main поменять название меню на своё."));
+                    return true;
+                }
                 var m = menu.create(player, null);
                 if (m instanceof HomeMenu homeMenu) {
                     homeMenu.setCustom(custom);
@@ -121,14 +153,27 @@ public class AhCommand implements CommandExecutor {
             default -> {
                 UUID uuid;
                 Player target = Bukkit.getPlayerExact(sub);
-                if (target != null) uuid = target.getUniqueId();
-                else uuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + sub).getBytes(StandardCharsets.UTF_8));
+                if (target != null) {
+                    uuid = target.getUniqueId();
+                } else {
+                    OfflinePlayer offline = Bukkit.getOfflinePlayer(sub);
+                    uuid = offline.getUniqueId();
+                }
                 User user = Main.getStorage().getUser(uuid);
-                if (user == null) { player.sendMessage(Color.translate("&cИгрок не найден!")); return true; }
+                if (user == null) {
+                    player.sendMessage(Color.translate(plugin.getConfig().getString("messages.no_player")));
+                    return true;
+                }
                 var menu = menuLoader.getMenu(viewMenuId);
-                if (menu == null) { player.sendMessage(Color.translate("&fЭх, опять… То ли я что-то намудрил, то ли ты забыл в Main поменять название меню на своё.")); return true; }
+                if (menu == null) {
+                    player.sendMessage(Color.translate("&fЭх, опять… То ли я что-то намудрил, то ли ты забыл в Main поменять название меню на своё."));
+                    return true;
+                }
                 var m = menu.create(player, null);
-                if (m instanceof PlayerItemsView view) { view.setUuid(uuid); view.setName(user.getNickName()); }
+                if (m instanceof PlayerItemsView view) {
+                    view.setUuid(uuid);
+                    view.setName(user.getNickName());
+                }
                 m.open();
                 return true;
             }
